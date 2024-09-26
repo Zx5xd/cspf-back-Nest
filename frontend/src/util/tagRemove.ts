7116e2c768087d54de2ba3af4496e406c5f76a60
Zetia <101085187+Zx5xd@users.noreply.github.com>
2024-09-26 10:30:54 +0900

    Delete frontend directory
    존재하지 않는 프로젝트

--- frontend/src/util/tagRemove.ts
+++
@@ -1,5 +0,0 @@
-export function TagRemove(obj: string | any): string {
-    if (typeof obj === 'string')
-        return obj.replace(/(<([^>]+)>)/ig, "").replace(/"n/, "").replace(/&amp;/g, "").replace(/&quot;/g, "");
-    else return obj;
-}
